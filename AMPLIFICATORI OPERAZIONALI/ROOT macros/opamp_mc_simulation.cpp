2e5466d332d7b432ac54bff53f4cc4e9acce602b
Nik <62773963+niklai99@users.noreply.github.com>
2020-11-03 16:58:32 +0100

    Completed MC simulation

--- AMPLIFICATORI OPERAZIONALI/ROOT macros/opamp_mc_simulation.cpp
+++ AMPLIFICATORI OPERAZIONALI/ROOT macros/opamp_mc_simulation.cpp
@@ -1,4 +1,4 @@
-using namfuncace std;
+using namespace std;
 #include<cmath>
 
 
@@ -19,13 +19,13 @@ void opamp_mc_simulation()
     double i = 0;
     while(f >> i) {
 
-        NSP::x.push_back(i);    //prima colonna
+        x.push_back(i);    //prima colonna
         f >> i;
-        NSP::y.push_back(i);    //seconda colonna
+        y.push_back(i);    //seconda colonna
         f >> i;
-        NSP::errX.push_back(i);     //push_back(0) se non ho errori sull'asse x
+        errX.push_back(i);     //push_back(0) se non ho errori sull'asse x
         f >> i;
-        NSP::errY.push_back(i);
+        errY.push_back(i);
 
     }
     f.close();
@@ -55,10 +55,10 @@ void opamp_mc_simulation()
 
     //mc preliminars
 
-    double offset = f1->GetParameter(0);
-    double err_offset = 0;
-    double slope = f1->GetParameter(1);
-    double err_slope = 0;
+    double q = f1->GetParameter(0);
+    double err_q = 0;
+    double m = f1->GetParameter(1);
+    double err_m = 0;
 
     TF1 *func = new TF1("func", "-0.0115109 + 9.98815*x", XMIN, XMAX);
 
@@ -83,77 +83,59 @@ void opamp_mc_simulation()
     {
         for (int i = 0; i < 2000; i++)
         {
-            toyV[i][j]= g1->Gaus(mcVout[j], mcVout_err[j]);
-            toyT[i][j]= g2->Gaus(mcVin[j], mcVin_err[j]);
+            toyVout[i][j]= g1->Gaus(mcVout[j], mcVout_err[j]);
+            toyVin[i][j]= g2->Gaus(mcVin[j], mcVin_err[j]);
         }
         
     }
-    
-
-   //linearizzazione dei toyV
-
-   Double_t logtoyV[2000][8];
-
-   for (int q = 0; q < 8; q++)
-   {
-       for (int p = 0; p < 2000; p++)
-       {
-           logtoyV[p][q]=log(toyV[p][q]);
-       }
-       
-   }
-
 
    //I FIT
 
     Double_t slope[2000] = {};
     Double_t errslope[2000] = {};
     Double_t relslope[2000] = {};
-    Double_t taus[2000] = {};
-    Double_t intercept[2000] = {};
-    Double_t ipsilon[8] = {};
-    Double_t ics[8] = {};
+    Double_t offset[2000] = {};
+    Double_t ipsilon[16] = {};
+    Double_t ics[16] = {};
+
+    TH1D *hist = new TH1D("slope distr", "Distribuzione di slope; slope; counts", 50, 9.984, 9.992);
+    TH1D *hist1 = new TH1D("#sigma_{slope}/slope distr", "Distribuzione di #sigma_{slope}/slope; #sigma_{slope}/slope; counts", 50, 0.0072, 0.0076);
 
     
     for (int l = 0; l < 2000; l++)
     {
-        for (int m = 0; m < 8; m++)
+        for (int m = 0; m < 16; m++)
         {
-            ics[m] = toyT[l][m];
-            ipsilon[m] = logtoyV[l][m];
+            ics[m] = toyVin[l][m];
+            ipsilon[m] = toyVout[l][m];
+            //cout << ics[m] << "\n" << ipsilon[m] << "\n\n\n";
         }
 
-        TGraph *graph = new TGraph(8,ics,ipsilon);
+        TGraphErrors *graph = new TGraphErrors(16, ics, ipsilon, mcVin_err, mcVout_err);
         TF1 *fit = new TF1("fit", "[0]+[1]*x",-10,300);  
-        fit->SetParNames("logV0","-1/TAU");
-        //fit->SetParameter(0,3);
-        //fit->SetParLimits(0,2.5,3.5);
-        //fit->SetParameter(1,-0.019);
-        //fit->SetParLimits(1,-0.03,-0.01);
-        TFitResultPtr res = graph-> Fit("fit", "S");
+        //fit->SetParNames("offset", "slope");
+  
+        //TFitResultPtr res = graph-> Fit("fit", "SQ");
+        graph-> Fit("fit", "Q");
 
-        intercept[l]=fit->GetParameter(0);
+        offset[l]=fit->GetParameter(0);
         slope[l]=fit->GetParameter(1);
         errslope[l]=fit->GetParError(1);
-        taus[l]=-1/(slope[l]);
         relslope[l] = errslope[l]/abs(slope[l]);
         //cout << relslope[l] << endl;
+
+        hist->Fill(slope[l]);
+        hist1->Fill(relslope[l]);
+
     }
- /*
- for (int test=0;test<8;test++){
-
-     cout << [test] << endl;
- } 
-*/
-
-    TH1D *hist = new TH1D("#tau distr", "Distribuzione di #tau; #tau (#mus); counts", 50, 128,140);
-
-    for(int filling=0; filling<2000; filling++){
-        hist->Fill(taus[filling]);
-    }
-    hist->Draw();
 
     hist->Fit("gaus");
+    hist1->Fit("gaus");
+
+
+    hist->Draw();
+
+    
 
     gPad->Modified();
     hist->GetXaxis()->SetTickLength(0.02);
@@ -163,14 +145,9 @@ void opamp_mc_simulation()
 
     c1->cd(2);
 
-    TH1D *hist1 = new TH1D("#sigma_{m}/m distr", "Distribuzione di #sigma_{m}/m; #sigma_{m}/m; counts", 50, 0,0.02);
-
-    for(int filling1=0; filling1<2000; filling1++){
-        hist1->Fill(relslope[filling1]);
-    }
     hist1->Draw();
 
-    hist1->Fit("gaus");
+    
 
     gPad->Modified();
     hist1->GetXaxis()->SetTickLength(0.02);
@@ -178,7 +155,7 @@ void opamp_mc_simulation()
 
     hist1->GetXaxis()->SetNdivisions(509, kTRUE);
     
-
+    c1->SaveAs("../Plots/opamp_mc.png");
 
 }
 
