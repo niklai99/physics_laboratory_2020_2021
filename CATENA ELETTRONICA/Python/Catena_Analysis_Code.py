53fb95a2070d2e901614ac0b508fdb48ceb9421a
Nik <62773963+niklai99@users.noreply.github.com>
2020-12-01 16:25:09 +0100

    Started Catena - Analysis

---
+++ CATENA ELETTRONICA/Python/Catena_Analysis_Code.py
@@ -0,0 +1,13 @@
+# CODE BY NICOLO LAI
+
+# IMPORT MODULES
+
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+import seaborn as sns
+import numpy as np
+from scipy import stats
+from scipy.optimize import curve_fit
+from scipy.misc import derivative
